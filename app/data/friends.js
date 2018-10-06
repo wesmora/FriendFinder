99ff12ebc9f3ff28e9e55510dbcf661f86988afc
Wes Mora <wesmora@gmail.com>
2018-10-05 19:47:31 -0600

    first push

---
+++ app/data/friends.js
@@ -0,0 +1,85 @@
+var friends = [
+
+    {
+        name: "Ahmed",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/1bd/3435aa3.jpg",
+        scores:[
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1",
+        ]
+    },
+    {
+        name: "Lou Ritter",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/1bd/3435aa3.jpg",
+        scores:[
+            "5",
+            "1",
+            "5",
+            "1",
+            "4",
+            "4",
+            "2",
+            "5",
+            "4",
+            "1",
+        ]
+    },
+    {
+        name: "Jordan Biason",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/1bd/3435aa3.jpg",
+        scores:[
+            "5",
+            "1",
+            "4",
+            "2",
+            "5",
+            "4",
+            "1",
+            "4",
+            "5",
+            "1",
+           
+        ]
+    },
+    {
+        name: "Mike",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/1bd/3435aa3.jpg",
+        scores:[
+            "5",
+            "4",
+            "4",
+            "5",
+            "4",
+            "1",
+            "1",
+            "1",
+            "2",
+            "5",
+        ]
+    },
+    {
+            name: "Rick",
+            photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/1bd/3435aa3.jpg",
+            scores:[
+                "5",
+                "3",
+                "4",
+                "5",
+                "4",
+                "1",
+                "5",
+                "1",
+                "2",
+                "1",
+            ]
+        },
+    
+]
